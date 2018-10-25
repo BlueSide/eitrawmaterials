96139f3e8a8ecfd724f6e4b62e5c348807256cb4
cikzh <marlonp33@gmail.com>
2018-10-25 14:41:15 +0200

    implemented basic structure of themefilter

--- src/app/theme-switcher/theme-switcher.component.ts
+++ src/app/theme-switcher/theme-switcher.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { ThemeService } from '../theme.service';
+import { Component } from '@angular/core';
+import { ThemeService, Theme } from '../theme.service';
 
 @Component({
     selector: 'theme-switcher',
     templateUrl: './theme-switcher.component.html',
     styleUrls: ['./theme-switcher.component.scss']
 })
-export class ThemeSwitcherComponent implements OnInit
+export class ThemeSwitcherComponent
 {
 
     constructor(private themes: ThemeService) { }
 
-    ngOnInit()
+    public toggleTheme(theme: Theme)
     {
+        theme.filter.enabled = !theme.filter.enabled;
     }
-
 }
